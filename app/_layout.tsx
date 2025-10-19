bac21638c528b96b482fb4092c5e7879122a220d
Alabi Temiloluwa Emmanuel <temiloluwaalabi33@gmail.com>
2025-10-19 13:59:29 +0100

    feat: auth login screen created

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,5 +19,11 @@ export default function RootLayout() {
   }, [fontsLoaded]);
 
   if (!fontsLoaded) return null;
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,51 @@
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignIn = () => {
+  const handleLogin = async () => {
+    console.log("LOGIN");
+  };
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+
+        <View className="px-10">
+          <Text className="text-base text-center font-rubik text-black-200 uppercase">
+            Welcome to ReState
+          </Text>
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let&apos; get you Closer to {" \n"}
+            <Text className="text-primary-300">Your Idea Home</Text>
+          </Text>
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to ReState with Google
+          </Text>
+          <TouchableOpacity
+            className="bg-gray-100 shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+            onPress={handleLogin}
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                resizeMode="contain"
+                className="size-5"
+              />
+              <Text className="text-lg font-rubik-medium text-black 300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
